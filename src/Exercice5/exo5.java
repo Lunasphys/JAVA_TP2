5dc1a440c858273330a5e2c026d369f89a733028
MaxYnovobry <maxime-obry@hotmail.fr>
2022-10-11 17:46:00 +0200

    blabla

--- src/Exercice5/exo5.java
+++ src/Exercice5/exo5.java
@@ -2,6 +2,8 @@ package src.Exercice5;
 
 import java.util.Scanner;
 import java.util.Random;
+
+import src.Menu;
 import src.Utils.utils;
 
 import static src.Utils.utils.entierInt;
@@ -28,54 +30,52 @@ public class exo5 {
         }
         System.out.println("Vous avez choisi de jouer avec " + nbAllumettes + " allumettes");
         System.out.println("Le joueur qui commence est choisi aléatoirement (1 pour vous, 2 pour l'ordinateur)");
-        int whoPlay = src.Utils.utils.getRandomNumber(1, 2);
+        int whoPlay = src.Utils.utils.getRandomNumber(1, 3);
         System.out.println("C'est le chiffre " + whoPlay + " qui commence");
-        if (whoPlay == 1) {
-            System.out.println("Vous commencez");
+
             while (nbAllumettes > 0) {
-                System.out.println("Il reste " + nbAllumettes + " allumettes");
-                System.out.println("Combien d'allumettes souhaitez vous retirer ? (1, 2 ou 3)");
-                int nbRetirer = entierInt();
-                if (nbRetirer > 3 || nbRetirer < 1) {
-                    System.out.println("Vous ne pouvez retirer que 1, 2 ou 3 allumettes");
-                    nbRetirer = entierInt();
-                } else if (nbRetirer > nbAllumettes-1) {
-                    System.out.println("Vous ne pouvez pas retirer plus d'allumettes que ce qu'il y en a");
-                    nbRetirer = entierInt();
-                } else {
-                    nbAllumettes = nbAllumettes - nbRetirer;
-                    if (nbAllumettes == 1) {
-                        System.out.println("Vous avez gagné !");
-                        break;
-                    }
+                if (whoPlay % 2 != 0) {
+                    System.out.println("A vous de jouer");
                     System.out.println("Il reste " + nbAllumettes + " allumettes");
+                    System.out.println("Combien d'allumettes souhaitez vous retirer ? (1, 2 ou 3)");
+                    int nbRetirer = entierInt();
+                    if (nbRetirer > 3 || nbRetirer < 1) {
+                        System.out.println("Vous ne pouvez retirer que 1, 2 ou 3 allumettes");
+                    } else if (nbRetirer > nbAllumettes ) {
+                        System.out.println("Vous ne pouvez pas retirer plus d'allumettes que ce qu'il y en a");
+                    } else {
+                        nbAllumettes = nbAllumettes - nbRetirer;
+                    }
+                    whoPlay += 1;
+                    System.out.println("Il reste " + nbAllumettes + " allumettes");
+                }
+                    else if(whoPlay % 2 == 0) {
                     // l'ordinateur doit jouer et doit laisser au joueur un nombre d'allumettes qui sera un multiple de 4
-                    Random rand = new Random();
-                    int nbAllumettesRetireesOrdi ;
-                    if (nbAllumettes % 4 == 3){
+                    System.out.println("A l'ordinateur de jouer");
+                    int nbAllumettesRetireesOrdi;
+                    if (nbAllumettes % 4 == 0) {
                         System.out.println("L'ordinateur retire 3 allumettes");
                         nbAllumettes -= 3;
-                    }
-                    else if (nbAllumettes % 4 == 2){
-                        System.out.println("L'ordinateur retire 2 allumettes");
-                        nbAllumettes -= 2;
-                    }
-                    else if (nbAllumettes % 4 == 1){
+                    } else if (nbAllumettes % 4 == 1) {
+                        System.out.println("L'ordinateur retire 1 allumette");
+                        nbAllumettes -= 1;
+                    } else if (nbAllumettes % 4 == 2) {
                         System.out.println("L'ordinateur retire 1 allumettes");
                         nbAllumettes -= 1;
-                    }
-                    else {
+                    } else {
+                        Random rand = new Random();
                         nbAllumettesRetireesOrdi = rand.nextInt(3) + 1;
                         nbAllumettes -= nbAllumettesRetireesOrdi;
-
+                        System.out.println("L'ordinateur retire " + nbAllumettesRetireesOrdi + "allumettes");
                     }
-
+                    whoPlay += 1;
+                }
                 }
             }
 
         }
-    }
-}
+
+
 
 
 
