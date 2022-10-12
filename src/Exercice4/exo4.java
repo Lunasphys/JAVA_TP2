48eb373eee3e8532e4b8553b91f1e9fd127d3f4f
MaxYnovobry <94967857+MaxYnovobry@users.noreply.github.com>
2022-10-12 16:55:09 +0200

    final

--- src/Exercice4/exo4.java
+++ src/Exercice4/exo4.java
@@ -19,8 +19,9 @@ public class exo4 {
         System.out.println("-----Exercice 4 -----");
         System.out.println("1 - Choississez la fonction principale de l'exercice avec des inputs");
         System.out.println("2 - Choississez seulement de factorisé");
-        System.out.println("3 - Testé seulement la fonction Cnp");
-        System.out.println("4 - Pour Quitter");
+        System.out.println("3 - Choississez la fonction principale avec multiplication");
+        System.out.println("4 - Testé seulement la fonction Cnp");
+        System.out.println("5 - Pour Quitter");
 
         int value = utils.entierInt();
         while (true) {
@@ -31,7 +32,7 @@ public class exo4 {
                     int n = utils.entierInt();
                     System.out.println("Entrer la valeur de p");
                     int p = utils.entierInt();
-                    System.out.println(Cnp(BigInteger.valueOf(n), BigInteger.valueOf(p)));
+                    System.out.println("la valeur de la methode 1 est " + Cnp(BigInteger.valueOf(n), BigInteger.valueOf(p)));
                     main(args);
                 case 2:
                     System.out.println("Vous avez choisi de factorisé un nombre");
@@ -40,16 +41,28 @@ public class exo4 {
                     System.out.println(factorize(BigInteger.valueOf(i)));
                     main(args);
                 case 3:
+                    System.out.println("Entrer la premiere valeur de n");
+                    int n2 = utils.entierInt();
+                    System.out.println("Entrer la premiere valeur de p");
+                    int p2 = utils.entierInt();
+                    System.out.println("Entrer la deuxieme valeur de n");
+                    int n3 = utils.entierInt();
+                    System.out.println("Entrer la deuxieme valeur de p");
+                    int p3 = utils.entierInt();
+                    BigInteger result1 = Cnp(BigInteger.valueOf(n2), BigInteger.valueOf(p2)).multiply(Cnp(BigInteger.valueOf(n3), BigInteger.valueOf(p3)));
+                    System.out.println("Cnp(" + n2 + ", " +p2 + ") x Cnp(" + n3 + ", " +p3 + ") = " + result1);
+                    main(args);
+                case 4 :
                     BigInteger result = Cnp(BigInteger.valueOf(50), BigInteger.valueOf(5)).multiply(Cnp(BigInteger.valueOf(11), BigInteger.valueOf(2)));
                     System.out.println("Cnp(50,5) x Cnp(11,2) = " + result);
                     main(args);
-                case 4 :
+                case 5 :
                     System.out.println("Au revoir");
                     System.exit(0);
                     break;
                 //Test la fonction Cnp et ensuite faite le calcul Cnp(50,5) *Cnp(11,2) il doit être égale a 116 531 800
             }
-            System.out.println("Veuillez entrer un nombre entre 1 et 4");
+            System.out.println("Veuillez entrer un nombre entre 1 et 5");
             main(args);
         }
     }
